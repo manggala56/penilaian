4f5888f00971a65ed4e5ebf2bf2848e6f3da54e8
manggala56 <manggalaputra740@gmail.com>
2025-11-14 19:03:25 +0700

    new update juri

--- app/Filament/Resources/JuriResource/Pages/CreateJuri.php
+++ app/Filament/Resources/JuriResource/Pages/CreateJuri.php
@@ -13,32 +13,23 @@ class CreateJuri extends CreateRecord
 {
     protected static string $resource = JuriResource::class;
 
-    protected function handleRecordCreation(array $data): Juri
+    protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // Data untuk membuat user baru
-        $userData = [
-            'name' => request('name'), // Ambil dari request langsung
-            'email' => request('email'),
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
             'role' => 'juri',
-            'password' => Hash::make(request('password')),
-        ];
+            'password' => Hash::make($data['password']),
+        ]);
 
-        // Create user account first
-        $user = User::create($userData);
-
-        // Jika juri universal, set category_id menjadi null
         if ($data['can_judge_all_categories']) {
             $data['category_id'] = null;
         }
 
-        // Then create juri record
-        return Juri::create([
-            'user_id' => $user->id,
-            'category_id' => $data['category_id'],
-            'expertise' => $data['expertise'],
-            'max_evaluations' => $data['max_evaluations'],
-            'is_active' => $data['is_active'],
-            'can_judge_all_categories' => $data['can_judge_all_categories'],
-        ]);
+        $data['user_id'] = $user->id;
+
+        unset($data['name'], $data['email'], $data['password']);
+
+        return $data;
     }
 }
