bca236d72ae935972d0b6f8a4255ee24a8bc7ac3
manggala56 <manggalaputra740@gmail.com>
2025-11-14 20:54:01 +0700

    New user id

--- app/Filament/Resources/EvaluationResource.php
+++ app/Filament/Resources/EvaluationResource.php
@@ -11,6 +11,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class EvaluationResource extends Resource
 {
