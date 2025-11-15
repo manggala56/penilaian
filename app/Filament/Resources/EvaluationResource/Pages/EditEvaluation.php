dadb10f4d8fe435cd2dd95948b640b5548bf2361
manggala56 <manggalaputra740@gmail.com>
2025-11-16 00:03:01 +0700

    new update

--- app/Filament/Resources/EvaluationResource/Pages/EditEvaluation.php
+++ app/Filament/Resources/EvaluationResource/Pages/EditEvaluation.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\EvaluationResource\Pages;
 use App\Filament\Resources\EvaluationResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-use App\Models\EvaluationScore;
+use App\Models\EvaluationScore; // <-- TAMBAHKAN IMPORT INI
 
 class EditEvaluation extends EditRecord
 {
@@ -17,11 +17,13 @@ class EditEvaluation extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    // Fungsi ini untuk MEMUAT data repeater saat halaman Edit dibuka
     protected function afterFill(): void
     {
         $evaluation = $this->getRecord();
 
-        // Load relasi scores beserta aspeknya
+        // Pastikan relasi 'scores' di Model/Evaluation Anda ada
         $scores = $evaluation->scores()->with('aspect')->get();
 
         // Format data untuk repeater
@@ -40,15 +42,16 @@ class EditEvaluation extends EditRecord
         ]);
     }
 
-    // TAMBAHKAN FUNGSI INI UNTUK MENYIMPAN data repeater
+    // Fungsi ini untuk MENYIMPAN data repeater saat di-edit
     protected function afterSave(): void
     {
         $evaluation = $this->getRecord();
         $scoresData = $this->form->getState()['scores'] ?? [];
 
-        // Cara termudah untuk sinkronisasi: Hapus yang lama, buat yang baru
+        // Hapus data lama
         $evaluation->scores()->delete();
 
+        // Buat data baru dari form
         foreach ($scoresData as $score) {
             EvaluationScore::create([
                 'evaluation_id' => $evaluation->id,
