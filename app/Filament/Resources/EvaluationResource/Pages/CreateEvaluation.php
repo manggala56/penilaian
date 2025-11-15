1cc0a35fc431adc05d3fa619484846f16cdb8e95
manggala56 <manggalaputra740@gmail.com>
2025-11-15 23:14:57 +0700

    new update

--- app/Filament/Resources/EvaluationResource/Pages/CreateEvaluation.php
+++ app/Filament/Resources/EvaluationResource/Pages/CreateEvaluation.php
@@ -13,16 +13,14 @@ use App\Models\EvaluationScore;
 class CreateEvaluation extends CreateRecord
 {
     protected static string $resource = EvaluationResource::class;
+
     public function mount(): void
     {
         parent::mount();
         $participantId = request()->query('participant_id');
 
-        \Log::info('CreateEvaluation mounted', ['participant_id' => $participantId]);
-
         if ($participantId) {
             $participant = Participant::with('category')->find($participantId);
-            \Log::info('Participant found', ['participant' => $participant]);
 
             if ($participant && $participant->category_id) {
                 $categoryId = $participant->category_id;
@@ -30,8 +28,6 @@ class CreateEvaluation extends CreateRecord
                 $aspects = Aspect::where('category_id', $categoryId)
                                  ->orderBy('id')
                                  ->get();
-
-                \Log::info('Aspects found', ['aspects_count' => $aspects->count(), 'category_id' => $categoryId]);
 
                 $scoresData = [];
                 foreach ($aspects as $aspect) {
@@ -43,8 +39,6 @@ class CreateEvaluation extends CreateRecord
                     ];
                 }
 
-                \Log::info('Scores data prepared', ['scores_data' => $scoresData]);
-
                 $this->form->fill([
                     'participant_id' => $participant->id,
                     'category_name' => $participant->category?->name ?? '',
@@ -52,8 +46,6 @@ class CreateEvaluation extends CreateRecord
                     'evaluation_date' => now(),
                     'scores' => $scoresData,
                 ]);
-
-                \Log::info('Form filled', ['form_data' => $this->form->getState()]);
             }
         }
     }
